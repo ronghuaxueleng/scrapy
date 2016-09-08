6e95407b3fc60f6fd086f20ea8d5737263360a86
ronghuaxueleng <caoqianghappy@126.com>
2016-09-08 18:15:32 +0800

    添加了文件

--- jianshuHot/start.sh
+++ jianshuHot/start.sh
@@ -3,6 +3,8 @@
 #source /etc/profile
 #source ~/.virtualenvs/spider/bin/activate
 
+path=`pwd`
+
 echo "删除旧数据 - note"
 python -c "from jianshu.db import delete_note; delete_note()"
 
@@ -25,19 +27,20 @@ echo "生成markdown"
 python -c "from jianshu.book import gen_markdown; gen_markdown()"
 
 echo "生成gitbook"
-cd output &&  gitbook mobi
+cd output && gitbook mobi
 
 echo "处理文件"
+cd $path
 cp output/book.mobi output/books/jianshu_hot-$(date "+%Y%m%d").mobi
 cp output/book.mobi output/books/jianshu_hot-latest.mobi
 echo "完成"
 
 
 ## 自定义邮箱服务器
-YOURKINDLE_MAIL_ADDRESS="xxxxx@kindle.cn"
-YOUR_SEND_MAIL_USERNAME="xxxx@163.com"
-YOUR_SEND_MAIL_SECRET = 'xxxxxxxxxxxx'
-MOBI_BOOK_PATH='./output/jianshu_hot-latest.mobi'
+YOURKINDLE_MAIL_ADDRESS="xxx@kindle.cn"
+YOUR_SEND_MAIL_USERNAME="xxx@126.com"
+YOUR_SEND_MAIL_SECRET='xxx'
+MOBI_BOOK_PATH=$path'/output/books/jianshu_hot-latest.mobi'
 
 ## 定义sendemail命令地址
-/usr/local/bin/sendEmail -s smtp.163.com -t $YOURKINDLE_MAIL_ADDRESS -u "简书热门$(date "+%Y%m%d")" -m "简书热门$(date "+%Y%m%d")" -xu $YOUR_SEND_MAIL_USERNAME -xp $YOUR_SEND_MAIL_SECRET -f $YOUR_SEND_MAIL_USERNAME -a $MOBI_BOOK_PATH
+$path/sendEmail/sendEmail.pl -s smtp.163.com -t $YOURKINDLE_MAIL_ADDRESS -u "简书热门$(date "+%Y%m%d")" -m "简书热门$(date "+%Y%m%d")" -xu $YOUR_SEND_MAIL_USERNAME -xp $YOUR_SEND_MAIL_SECRET -f $YOUR_SEND_MAIL_USERNAME -a $MOBI_BOOK_PATH
